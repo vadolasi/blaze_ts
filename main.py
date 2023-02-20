b043c5ce8318f6528d73c11c96e9f3cc261ce9e0
Vitor Daniel <vitor036daniel@gmail.com>
2023-02-19 22:10:59 -0300

    fix

--- main.py
+++ main.py
@@ -1,79 +1,140 @@
 import time
+import pathlib
+from typing import Union
+import asyncio
 
 import httpx
 import parsel
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(f"Inscrição ativada, aguarde para receber as notificações.")
+listners_path = pathlib.Path("./listners.txt")
 
-app = ApplicationBuilder().token("5873581129:AAG_3zua9tlXxJDAuqDcvxZE-puUTCRRQIs").build()
+running = False
 
-app.add_handler(CommandHandler("start", start))
 
-last_id = None
-last_white_distance = 0
+async def main():
+    last_id = None
+    last_white_distance = 0
 
-sum_2_3 = None
-sum_3_4 = None
-sum_1_3 = None
-sim_3_5 = None
-sum_2_3_4 = None
+    soma_esquerdo_1: Union[int, None] = None
+    soma_direito_1: Union[int, None] = None
+    soma_2_lados_1: Union[int, None] = None
+    soma_2_esquerdo_1: Union[int, None] = None
+    soma_2_direito_1: Union[int, None] = None
+    soma_esquerdo_2: Union[int, None] = None
+    soma_direito_2: Union[int, None] = None
+    soma_2_lados_2: Union[int, None] = None
+    soma_2_esquerdo_2: Union[int, None] = None
+    soma_2_direito_2: Union[int, None] = None
 
-app.run_polling()
+    while True:
+        try:
+            response = httpx.get(
+                "https://historicosblaze.com/br/blaze/doubles",
+                headers={
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+                }
+            )
 
-while True:
-    try:
-        response = httpx.get(
-            "https://historicosblaze.com/br/blaze/doubles",
-            headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-            }
-        )
+            response.raise_for_status()
+            selector = parsel.Selector(response.text)
 
-        response.raise_for_status()
-        selector = parsel.Selector(response.text)
+            item = selector.css(".double-single")[0]
 
-        item = selector.css(".double-single")[0]
+            item_id = item.xpath("./@data-id").get()
 
-        item_id = item.xpath("./@data-id").get()
+            if item_id == last_id:
+                continue
 
-        if item_id == last_id:
-            continue
+            last_id = item_id
 
-        last_id = item_id
+            item_number = int(item.css(".number-table::text").get())
+            print(item_number, last_white_distance)
 
-        item_number = int(item.css(".number-table::text").get())
+            if item_number == 0:
+                last_white_distance = 1
+                continue
+            elif last_white_distance == 1:
+                last_white_distance = 2
+            elif last_white_distance == 2:
+                last_white_distance = 0
 
-        if item_number == 0:
-            last_white_distance += 1
-            continue
-        elif last_white_distance == 2:
-            last_white_distance = 0
+                items = selector.css(".double-single")
 
-            items = selector.css(".double-single")
-
-            if sum_2_3:
                 item1_number = int(items[0].css(".number-table::text").get())
                 item2_number = int(items[1].css(".number-table::text").get())
                 item3_minutes = int(items[2].css(".minute-table::text").get().split(":")[0])
                 item4_number = int(items[3].css(".number-table::text").get())
                 item5_number = int(items[4].css(".number-table::text").get())
 
+                soma_esquerdo_2 = item2_number + item3_minutes
+                soma_direito_2 = item3_minutes + item4_number
+                soma_2_lados_2 = item2_number + item3_minutes + item4_number
+                soma_2_esquerdo_2 = item1_number + item2_number + item3_minutes
+                soma_2_direito_2 = item3_minutes + item4_number + item5_number
+
                 if item2_number > 10:
                     item2_number =  int(str(item2_number)[0]) + int(str(item2_number)[1])
-                
+
                 if item4_number > 10:
                     item4_number =  int(str(item4_number)[0]) + int(str(item4_number)[1])
 
-                sum_2_3 = item2_number + item3_minutes
-                sum_3_4 = item3_minutes + item4_number
-                sum_1_3 = item1_number + item3_minutes
-                sim_3_5 = item3_minutes + item5_number
-                sum_2_3_4 = item2_number + item3_minutes + item4_number
+                soma_esquerdo_1 = item2_number + item3_minutes
+                soma_direito_1 = item3_minutes + item4_number
+                soma_2_lados_1 = item2_number + item3_minutes + item4_number
+                soma_2_esquerdo_1 = item1_number + item2_number + item3_minutes
+                soma_2_direito_1 = item3_minutes + item4_number + item5_number
+            
+                for listner in listners_path.read_text().strip().split("\n"):
+                    listner = int(listner.strip())
 
-    except Exception as e:
-        print(e)
+                    await app.updater.bot.send_message(
+                        listner,
+                        f"""🔮Estamos sentido a presença do branco🔮
 
-    time.sleep(15)
+{soma_esquerdo_1}
+{soma_direito_1}
+{soma_2_lados_1}
+{soma_2_esquerdo_1}
+{soma_2_direito_1}
+
+Minutos de recuperação 
+{soma_direito_2}
+{soma_esquerdo_2}
+{soma_2_lados_2}
+{soma_2_esquerdo_2}
+{soma_2_direito_2}
+
+Boa sorte 🤑""")
+
+        except Exception as e:
+            print(e)
+    
+        await asyncio.sleep(15)
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global running
+
+    if not running:
+        running = True
+        asyncio.create_task(main())
+        print("Running...")
+
+    listners = listners_path.read_text().strip().split("\n")
+
+    if str(update.message.chat_id) in listners:
+        await update.message.reply_text(f"Você já está inscrito.")
+        return
+
+    listners_path.write_text(listners_path.read_text() + f"{update.message.chat_id}\n", encoding="utf-8")
+    listners_path.write_text(listners_path.read_text().strip())
+
+    await update.message.reply_text(f"Inscrição ativada, aguarde para receber as notificações.")
+
+app = ApplicationBuilder().token("5873581129:AAG_3zua9tlXxJDAuqDcvxZE-puUTCRRQIs").build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
