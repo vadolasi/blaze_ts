3325e1971749772736469cce90842bc77b060e29
Vitor Daniel <vitor036daniel@gmail.com>
2023-02-20 11:08:16 -0300

    fix

--- main.py
+++ main.py
@@ -52,6 +52,13 @@ async def main():
             item_number = int(item.css(".number-table::text").get())
 
             if item_number == 0:
+                item_minute = int(item.css(".minute-table::text").get().split(":")[1])
+                
+                if item_minute > soma_esquerdo_1 - 1 and item_minute < soma_esquerdo_1 + 1 or item_minute > soma_direito_1 - 1 and item_minute < soma_direito_1 + 1 or item_minute > soma_2_lados_1 - 1 and item_minute < soma_2_lados_1 + 1 or item_minute > soma_2_esquerdo_1 - 1 and item_minute < soma_2_esquerdo_1 + 1 or item_minute > soma_2_direito_1 - 1 and item_minute < soma_2_direito_1 + 1 or item_minute > soma_esquerdo_2 - 1 and item_minute < soma_esquerdo_2 + 1 or item_minute > soma_direito_2 - 1 and item_minute < soma_direito_2 + 1 or item_minute > soma_2_lados_2 - 1 and item_minute < soma_2_lados_2 + 1 or item_minute > soma_2_esquerdo_2 - 1 and item_minute < soma_2_esquerdo_2 + 1 or item_minute > soma_2_direito_2 - 1 and item_minute < soma_2_direito_2 + 1:
+                    await app.updater.bot.send_message("Paga blaze")
+                else:
+                    await app.updater.bot.send_message("❌❌❌❌❌❌")
+
                 last_white_distance = 1
                 continue
             elif last_white_distance == 1:
@@ -63,7 +70,7 @@ async def main():
 
                 item1_number = int(items[0].css(".number-table::text").get())
                 item2_number = int(items[1].css(".number-table::text").get())
-                item3_minutes = int(items[2].css(".minute-table::text").get().split(":")[0])
+                item3_minutes = int(items[2].css(".minute-table::text").get().split(":")[1])
                 item4_number = int(items[3].css(".number-table::text").get())
                 item5_number = int(items[4].css(".number-table::text").get())
 
@@ -74,10 +81,10 @@ async def main():
                 soma_2_direito_2 = item3_minutes + item4_number + item5_number
 
                 if item2_number > 10:
-                    item2_number =  int(str(item2_number)[0]) + int(str(item2_number)[1])
+                    item2_number = int(str(item2_number)[0]) + int(str(item2_number)[1])
 
                 if item4_number > 10:
-                    item4_number =  int(str(item4_number)[0]) + int(str(item4_number)[1])
+                    item4_number = int(str(item4_number)[0]) + int(str(item4_number)[1])
 
                 soma_esquerdo_1 = item2_number + item3_minutes
                 soma_direito_1 = item3_minutes + item4_number
